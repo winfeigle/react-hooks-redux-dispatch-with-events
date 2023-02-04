aafe64080bd0281dab18f6d78570c6ee54c71cb0
Win Feigle <win.feigle@gmail.com>
2023-02-04 12:54:33 -0700

    commit tree 24de25d566b069ac9d711d6e889d25ca4d9df1c8

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,29 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action){
+    switch (action.type) {
+        case "counter/increment":
+            return { count: state.count + 1}
+        default:
+            return state;
+    }
+}
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+const render = () => {
+    const container = document.querySelector('#container');
+    container.textContent = state.count
+}
+
+dispatch({type: '@@init'});
+
+const button = document.querySelector('#button');
+
+button.addEventListener("click", () => {
+    dispatch({ type: "counter/increment" })
+});
